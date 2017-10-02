04b3f528eed3614293c96c4d3a74f67a1edd4402
Konstantin Bogdanov <kbogdanov1@gmail.com>
2017-10-03 02:06:42 +0300

    styles 2

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -1,46 +1,41 @@
 jQuery(document).ready(function($){
 
-  /* галерея "примеры" */
-  $('.examples__gallery').slick({
-    infinite: true,
-    dots: false,
-    arrows: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '0',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 
-  /* галерея "отзыы" */
-  $('.reviews__gallery').slick({
-    infinite: true,
-    dots: false,
-    arrows: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '0'
-  });
-
+  function initSlider() {
+    // галерея "отзывы"
+    $('.gallery__list').slick({
+      infinite: true,
+      dots: false,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '0'
+    });
+  }
 
 
   /* 404 - sticky footer */
   // var footerHeight = $('.footer').outerHeight();
   if ($(window).height() >= $('body').height()){
     $('.page').addClass('page--fixed');
-    // $('body').css('padding-bottom', footerHeight+'px');
     $('.footer').addClass('footer--fixed');
   }
 
+
+  /* Gallery in modal window */
+  $('.about-gallery__link').click( function(e){
+    e.preventDefault();
+    $('body').css({'overflow':'hidden'});
+    $('.overlay').show();
+    initSlider();
+  });
+  /* Close modal window */
+  $('.overlay__bg, .overlay__close').click( function(e){
+    e.preventDefault();
+    $('body').css({'overflow':'auto'});
+    $(this).closest('.overlay').fadeOut(400);
+  });
 
 
 
